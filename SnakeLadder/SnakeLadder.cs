6ececb97257d1209e853fe492f4135459862a498
SandeepSinghGaur <sandeepgaurdec13@gmail.com>
2021-01-22 02:32:02 +0530

    [Sandeep] Added:Player Reaches Final Position

--- SnakeLadder/SnakeLadder.cs
+++ SnakeLadder/SnakeLadder.cs
@@ -1,17 +1,17 @@
 ﻿using System;
-    class SnakeLadder
-    {
-    public static  int INTIAL_POSITION = 0;
-    public static  int FINAL_POSITION = 100;
+class SnakeLadder
+{
+    public static int INTIAL_POSITION = 0;
+    public static int FINAL_POSITION = 100;
     public static int NUMBER_OF_PLAYER = 1;
     public static int LADDER = 1;
     public static int SNAKE = 2;
     public static int NO_PLAY = 3;
-    public static int Option_Check(int position,int dice_value)
+    public static int Option_Check(int position, int dice_value)
     {
         Random random = new Random();
         int option = random.Next(1, 4);
-        switch(option)
+        switch (option)
         {
             case 1:
                 Console.WriteLine("Player getting Ladder");
@@ -22,30 +22,34 @@
                 if (position > 0)
                     position -= dice_value;
                 else
-                    position = 0;
+                    position = position;
                 break;
             case 3:
                 Console.WriteLine("Now Player is not Playing");
-                position = 0;
+                position = position;
                 break;
             default:
                 break;
         }
+        if (position < 0)
+            position = INTIAL_POSITION;
+        if (position > 100)
+            position = FINAL_POSITION;
         return position;
 
     }
 
     static void Main(string[] args)
+    {
+        int position = 0;
+        while (position < FINAL_POSITION)
         {
-        int position =0;
-        Random random = new Random();
-        int dice_value = random.Next(1, 7);
-        Console.WriteLine("Welcome to Snake and Ladder Game!");
-        Console.WriteLine("Game Start With the Intial Position:" + " " + INTIAL_POSITION);
-        Console.WriteLine("Start With the Player:" + " " + NUMBER_OF_PLAYER);
-        Console.WriteLine("Number Comes by rolling a Dice :" + " " + dice_value);
-        position =Option_Check(position, dice_value);
-        Console.WriteLine("Player at Position :" + " " + position);
+            Random random = new Random();
+            int dice_value = random.Next(1, 7);
+            position = Option_Check(position, dice_value);
+
+        }
+        Console.WriteLine("Player's Final Position :" + " " + position);
     }
-    }
+}
 
