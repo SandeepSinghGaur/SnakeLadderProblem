0838bd9d6fbdf0a5d1e3953e06c49187fdf0806b
SandeepSinghGaur <sandeepgaurdec13@gmail.com>
2021-01-22 01:45:50 +0530

    [Sandeep] Added: Getting Number By rolling the Dice

--- SnakeLadder/SnakeLadder.cs
+++ SnakeLadder/SnakeLadder.cs
@@ -5,9 +5,12 @@
         {
         int INTIAL_POSITION =0;
         int NUMBER_OF_PLAYER = 1;
+        Random random = new Random();
+        int dice_value = random.Next(1, 7);
         Console.WriteLine("Welcome to Snake and Ladder Game!");
         Console.WriteLine("Game Start With the Intial Position:" + " " + INTIAL_POSITION);
         Console.WriteLine("Start With the Player:" + " " + NUMBER_OF_PLAYER);
+        Console.WriteLine("Number got by rolling a Dice :" + " " + dice_value);
     }
     }
 
