c84756cc23d01a1428f07a52cd807a84340c0d52
SandeepSinghGaur <sandeepgaurdec13@gmail.com>
2021-01-22 01:36:28 +0530

    [Sandeep] Added:Fix the Intial Position and Player

---
+++ SnakeLadder/SnakeLadder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+    class SnakeLadder
+    {
+        static void Main(string[] args)
+        {
+        int INTIAL_POSITION =0;
+        int NUMBER_OF_PLAYER = 1;
+        Console.WriteLine("Welcome to Snake and Ladder Game!");
+        Console.WriteLine("Game Start With the Intial Position:" + " " + INTIAL_POSITION);
+        Console.WriteLine("Start With the Player:" + " " + NUMBER_OF_PLAYER);
+    }
+    }
+
