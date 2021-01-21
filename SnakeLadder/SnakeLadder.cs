9b3dead2c7087fc037e3eaccfd305d1db9fd9800
SandeepSinghGaur <sandeepgaurdec13@gmail.com>
2021-01-22 03:40:37 +0530

    [Sandeep] Added:Two Player Participate in the Game find Winner

--- SnakeLadder/SnakeLadder.cs
+++ SnakeLadder/SnakeLadder.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Collections;
 class SnakeLadder
 {
     public static int INTIAL_POSITION = 0;
     public static int FINAL_POSITION = 100;
-    public static int NUMBER_OF_PLAYER = 1;
+    public static int NUMBER_OF_PLAYER = 2;
     public static int LADDER = 1;
     public static int SNAKE = 2;
     public static int NO_PLAY = 3;
@@ -38,28 +37,38 @@ class SnakeLadder
         if (position < 0)
             position = INTIAL_POSITION;
         return position;
-
     }
-
-    static void Main(string[] args)
+    public static void Two_Player_Game()
     {
         int position = 0;
-        int count = 0;
-        ArrayList show_positions = new ArrayList();
+        int first_player_position = 0;
+        int position_count = 0;
+        int second_player_position = 0;
         while (position < FINAL_POSITION)
         {
             Random random = new Random();
-            int dice_value = random.Next(1, 7);
-            position = Option_Check(position, dice_value);
-            show_positions.Add(position);
-            count++;
-
+            int dice_value_first = random.Next(1, 7);
+            int dice_value_second = random.Next(1, 7);
+            first_player_position = Option_Check(first_player_position, dice_value_first);
+            second_player_position = Option_Check(second_player_position, dice_value_second);
+            if (first_player_position > second_player_position)
+                position = first_player_position;
+            else
+                position = second_player_position;
+            position_count++;
         }
-        Console.WriteLine("Player's Final Position :" + " " + position+" "+"Number of Time Dice Rolls"+" "+count);
-        for (int progress = 0; progress < count; progress++)
-        {
-            Console.WriteLine("Position after every roll" + " " + show_positions[progress]);
-        }
+        if (first_player_position > second_player_position)
+            Console.WriteLine("First Player is Winner");
+        else
+            Console.WriteLine("Second Player is Winner");
+        Console.WriteLine("Position of First Player" + " " + first_player_position);
+        Console.WriteLine("Position of Second Player" + " " + second_player_position);
+        Console.WriteLine("No.Of Times Dice Roll" + " " + position_count);
     }
+    static void Main(string[] args)
+    {
+        Two_Player_Game();
+    }
+
 }
 
